53c20eca78bebd23136a2886c069f16bfdb70c23
JoseLucasapp <josluc2016@gmail.com>
2020-09-26 19:59:20 -0300

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const cors = require('cors')
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,7 +9,7 @@ const read = () => {
     const data = fs.readFileSync('./jsonarchs/times.json','utf-8');
     return data;
 }
-
+app.use(cors({origin: '*'}))
 app.get('/times', (req,res)=>{
     const data = read();
     res.send(data);
