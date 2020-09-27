e42fab44fd0d051de21389d15e6516eb596f415e
JoseLucasapp <josluc2016@gmail.com>
2020-09-26 21:12:03 -0300

    update

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const read = () => {
     const data = fs.readFileSync('./jsonarchs/times.json','utf-8');
     return data;
 }
+
 app.use(cors({origin: '*'}))
+
 app.get('/times', (req,res)=>{
     const data = read();
     res.send(data);
