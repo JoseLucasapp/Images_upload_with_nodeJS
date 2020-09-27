041d1f2b95cce1280f2a6c18db877c15188ef4e6
JoseLucasapp <josluc2016@gmail.com>
2020-09-26 21:13:45 -0300

    update

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const cors = require('cors')
 const PORT = process.env.PORT || 3000;
 
 const read = () => {
-    const data = fs.readFileSync('./jsonarchs/times.json','utf-8');
+    const data = fs.readFileSync('./jsonarchs/data.json','utf-8');
     return data;
 }
 
 app.use(cors({origin: '*'}))
 
-app.get('/times', (req,res)=>{
+app.get('/', (req,res)=>{
     const data = read();
     res.send(data);
 });
