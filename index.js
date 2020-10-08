fa3f379a083fd40f56c2b91ff45d4f0eb97ac55a
JoseLucasapp <josluc2016@gmail.com>
2020-10-08 09:09:28 -0300

    add cors

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const PORT = process.env.PORT || 3000;
+const cors = require('cors');
 
 app.use(express.json({extended: true}));
+app.use(cors({origin: '*'}))
 
 const readfile = () => {
     const data = fs.readFileSync('./dbjson/cars.json','utf-8');
