35d5c1d4f342740f26afb298ae7461de50f2917b
JoseLucasapp <josluc2016@gmail.com>
2020-11-19 17:52:03 -0300

    add to git

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const fs = require('fs');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json({extended: true}));
 
