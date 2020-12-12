00149930c89ee7497afdf52043802b9b4f0b3cfd
José Lucas <58341307+JoseLucasapp@users.noreply.github.com>
2020-12-12 10:32:01 -0300

    Add files via upload

---
+++ comandosCmd.js
@@ -0,0 +1,14 @@
+console.log('date');//data
+console.log('time'); //hora
+console.log('dir /w');//mostrar as pastas em colchete
+console.log('clear');//limpar cursor
+console.log('mkdir');//criar pasta
+console.log('cd');//mudar diretório
+console.log('rd');// remove diretório limpo
+console.log('tree /f');//mostra diretórios organizados
+console.log('chkdsk'); //checa o disco rigido
+console.log('mem');//checa a memória
+console.log('ren nane.txt name.txt');//renomeia
+console.log('del');//deleta arquivos
+console.log('undelete');//volta atrás na deleção
+console.log('cls');//limpar console
